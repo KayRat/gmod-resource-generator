c5907ed7559692c4c63c11c39d1425189ca9a531
KayRat <kyle.ratti@outlook.com>
2014-10-19 01:43:00 -0400

    Removed old files

--- src/io/github/bananalord/gmodresourcegenerator/ExtraContentType.java
+++
@@ -1,7 +0,0 @@
-package io.github.bananalord.gmodresourcegenerator;
-
-public enum ExtraContentType {
-    ADDON,
-    GAMEMODE,
-    UNKNOWN,
-}
--- src/io/github/bananalord/gmodresourcegenerator/UnknownContentTypeException.java
+++
@@ -1,7 +0,0 @@
-package io.github.bananalord.gmodresourcegenerator;
-
-public class UnknownContentTypeException extends RuntimeException {
-    public UnknownContentTypeException(String str) {
-        super(str);
-    }
-}
